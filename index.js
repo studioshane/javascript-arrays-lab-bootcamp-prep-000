24a363348f638c5b312382c7001a958d8b47c030
Shane Walsh <studioshane@gmail.com>
2018-09-08 12:13:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your functions and code here
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(kittens, name) {
+ 
+}
+
+function destructivelyPrependKitten(name, array) {
+ 
+}
+
+function destructivelyRemoveLastKitten(array) {
+  
+}
+
+function destructivelyRemoveLastKitten(array) {
+  
+}
+
+function removeLastKitten(array) {
+  return array.slice(0, array.length -1);
+}
+
+function removeFirstKitten(array) {
+  return array.slice(1);
+}
