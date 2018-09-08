d685613e137f43718fbba1495b448fe99ec1ae79
Shane Walsh <studioshane@gmail.com>
2018-09-08 12:21:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ function destructivelyPrependKitten(name) {
  return kittens
 }
 
-function destructivelyRemoveLastKitten(name) {
-  kittens.pop(name)
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
   return kittens 
 }
 
-function destructivelyRemoveFirstKitten(name) {
-  kittens.shift(name)
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
   return kittens
 }
 
